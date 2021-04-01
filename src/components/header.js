0cee550b01483ddb712f47eafc222ef7c7f161f0
BrandonMok <brandonmok88@yahoo.com>
2021-04-01 10:37:59 -0400

    [WIP] Adding mobile nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,24 @@
 import logo from '../logo.png';
 import React from 'react';
+import $ from 'jquery';
 
 class Header extends React.Component {
+    openNav() {
+        alert("it works");
+    }
     render() {
         return (
             <div className="header d-flex justify-content-between flex-direction-row">
+
+
                 <div className="header__logo-row">
+                <span id="hamburger" onClick={this.openNav}>&#9776;</span>
                     <img src={logo} alt="logo" className="header__logo-row__image" />
                     <div className="header__logo-row__name">Brandon Mok</div>
                 </div>
                 <div className="header__menu-row">
+                    
+
                     <ul className="main-menu">
                         <li><a href="#about" alt="About">About</a></li>
                         <li><a href="#projects" alt="About">Projects</a></li>
