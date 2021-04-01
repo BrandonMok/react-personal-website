51e3e1feb909d9c6020a93d016a29ee82536c1f4
BrandonMok <brandonmok88@yahoo.com>
2021-03-31 20:26:02 -0400

    Added more to banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import BannerImg from '../icons/undraw_traveling.svg';
 
  class Banner extends React.Component {
     render() {
         return (
-            <div className="banner d-flex">
-                
+            <div className="banner d-flex justify-content-around align-items-center p-4">
+                <div className="banner__text">
+                    <div className="banner__text__title">Hello, I'm Brandon!</div>
+                </div>
+                <div className="banner__image">
+                    <img src={BannerImg} alt="" />
+                </div>
             </div>
          );
     }
