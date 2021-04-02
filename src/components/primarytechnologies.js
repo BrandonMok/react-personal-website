07a724886bfe700ec06bfa2ea4098fb4b37d3412
BrandonMok <brandonmok88@yahoo.com>
2021-04-02 17:17:12 -0400

    Added more to getting primary tech component styling

--- src/components/primarytechnologies.js
+++ src/components/primarytechnologies.js
@@ -5,15 +5,18 @@ class PrimaryTechnologies extends React.Component {
     render() {
         return (
             <div className="primary-tech">
-                {/* <div className="d-flex">
+                <div className="container">
                     <div className="row">
-                        <div className="primary-tech__title-content">
-                            <div className="primary-tech__title-content__title">Primary Technologies</div>
-                            <div className="primary-tech__title-content__line"></div>
+                        <div className="primary-tech__top">
+                            <div className="primary-tech__top__title">Primary Technologies</div>
+                            <div className="primary-tech__top__line"></div>
                         </div>
                     </div>
-                    <div className="row"></div>
-                </div> */}
+                    <div className="row">
+
+
+                    </div>
+                </div>
             </div>
         );
     }
