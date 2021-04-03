9eaf6f5dc6fc09daf52dd65d36d3cbcbc0247ae5
BrandonMok <brandonmok88@yahoo.com>
2021-04-03 09:15:23 -0400

    Added more icons

--- src/components/moreinfo.js
+++ src/components/moreinfo.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import GithubLogo from '../icons/github.svg';
+import LinkedInLogo from '../icons/linkedin.svg';
+import DocIcon from '../icons/cv.svg';
+
 
 class MoreInfo extends React.Component {
     render() {
@@ -10,6 +14,21 @@ class MoreInfo extends React.Component {
                             <div className="more-info__top">
                                 <div className="more-info__top__title">More Info</div>
                                 <div className="more-info__top__line"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="d-flex flex-wrap justify-content-center align-items-center more-info__bottom-container">
+                                <div className="more-info__bottom-container__box">
+                                    <img src={GithubLogo} alt="Github Logo" /> 
+                                </div>
+                                <div className="more-info__bottom-container__box">
+                                    <img src={LinkedInLogo} alt="LinkedIn Logo" /> 
+                                </div>
+                                <div className="more-info__bottom-container__box">
+                                    <img src={DocIcon} alt="Document icon" /> 
+                                </div>
                             </div>
                         </div>
                     </div>
