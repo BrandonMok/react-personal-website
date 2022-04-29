a45fc5b64483542682b1ad84339df7be20fe39c5
BrandonMok <brandonmok88@yahoo.com>
2022-04-28 20:20:35 -0400

    redid banner to use grid

--- src/components/banner.js
+++ src/components/banner.js
@@ -5,17 +5,17 @@ import AvatarPic from '../icons/avatar.svg'
 const Banner = () => {
     return (
         <div className="banner">
-            <div className="container banner__flexbox">
-                <div className='banner__flexbox__box1'>
-                    <div className='banner__flexbox__box1__headline-content'>
-                        <div className='banner__flexbox__box1__headline-content__subheadline'>Hello, I'm</div>
-                        <div className='banner__flexbox__box1__headline-content__headline'>Brandon Mok</div>
-                        <div className='banner__flexbox__box1__headline-content__subheadline'>Full-stack Developer</div>
-                    </div>        
-                </div>        
-                <div className='banner__flexbox__box2'>
-                    <img src={AvatarPic} alt='avatar' />
-                </div>                
+            <div className="container">
+                <div className='row'>
+                    <div className='col-12 col-md-6 banner__headline-intro'>
+                        <div className='banner__headline-intro__subheadline'>Hello, I'm</div>
+                        <div className='banner__headline-intro__headline'>Brandon Mok</div>
+                        <div className='banner__headline-intro__subheadline'>Full-stack Developer</div>
+                    </div>
+                    <div className='col-12 col-md-6 banner__avatar'>
+                        <img src={AvatarPic} alt='avatar' />
+                    </div>
+                </div>
             </div>
         </div>
     );
