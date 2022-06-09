3d2d09ec176a7e4684aa12d0cd15107d8fed84b7
brandonmok <brandonmok88@yahoo.com>
2022-06-09 10:53:41 -0400

    Redo (again) of about content

--- src/components/about.js
+++ src/components/about.js
@@ -15,12 +15,18 @@ export default function About() {
                     <div className='col-12 col-md-6 aboutme__content-col'>
                         <SectionTitle title="About Me" />
                         <div className='aboutme__content-col__content'>
-                            I'm Brandon, and I'm a full stack developer.
-                            I build custom solutions for content management systems and tackle business problems with Salesforce.
-                            My experience, however, spans across a diverse range of areas consisting of web development, OOP, CMS, CRM, databases, and frameworks. 
+                            I'm Brandon, and I'm a full stack developer based out of New York.
                             <br />
                             <br />
-                            Besides tech, some of my other interests involve building custom keyboards, working on cars, and learning more about other industries, trades, and skills.
+                            I graduated from the Rochester Institute of Technology in 2017 with a Bachelors in Web & Mobile Computing. 
+                            During my college career, I focused on becoming a LAMP developer with knowledge in building full stack web applications.
+                            My work can be found on my <a href="https://github.com/BrandonMok" alt="Github">GitHub</a> which consists anywhere from
+                            front and backend applications, APIs (JS & Java), Java networking, IOS and Android mobile apps, and more.
+                            <br />
+                            <br />
+                            Since college, I've quickly expanded and branched my knowledge to encompass a broader skillset. My full experience now involves
+                            web development, object-oriented programming, content management systems (Drupal & WordPress), customer relationship management tools (Salesforce), databases, and frameworks. 
+                            Additionally, most of my past production work experience comes from developing custom solutions for content management systems and solving business problems with Salesforce.
                         </div>
                     </div>
                 </div>
